27f7e24c6dcfde0a3793080db8cd8880a7645c4c
Prince Thind <Princethind799@gmail.com>
2022-04-04 13:53:04 +0530

    add fetch code

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 require("dotenv").config();
+const fetchRecords = require("./lib/fetchRecords");
+const falso = require("@ngneat/falso");
+
 const PORT = process.env.PORT;
 
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send('hello world')
+  let num = req.query.n ?? 15;
+  let name = req.query.name ?? falso.randProductName;
+
+  name = sanitiseName(name);
+  num = sanitiseNum(num);
+
+  fetchRecords(name, num)
+    .then((response) => {
+      res.json({ data: response, error: null });
+    })
+    .catch((error) => {
+      res.json({ data: null, error });
+    });
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}, visit http://localhost:${PORT}`);
 });
+
+function sanitiseName(name) {
+  let res = null;
+
+  if (typeof name !== "string") {
+    name = "";
+  }
+
+  if (name.length > 50) {
+    res = name.slice(0, 50);
+  }
+
+  return res;
+}
+
+function sanitiseNum(input) {
+  let num = 1;
+  if (typeof input !== "number") {
+    input = 1;
+  }
+
+  num = Math.round(input);
+
+  // num between 0-50
+  num = Math.min(50, input);
+  num = Math.max(0, num);
+  return num;
+}
