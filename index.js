c5fa113b3a36ff5935084cd2f4a54407d437d9ad
Prince Thind <princethind799@gmail.com>
2022-05-21 19:09:56 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const cors=require('cors');
+const cors = require("cors");
+const express = require("express");
+
 const fetchRecords = require("./lib/fetchRecords");
 
-const PORT = process.env.PORT||8001;
-
-const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8001;
 
-app.use(cors({
-  origin:process.env.ORIGIN,
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
-app.get("/items.json", (req, res) => {
+app.get("/", (req, res) => {
   let num = req.query.n ?? 15;
-
-  num = sanitiseNum(num);
 
   fetchRecords(num)
     .then((response) => {
@@ -22,25 +22,10 @@ app.get("/items.json", (req, res) => {
     })
     .catch((error) => {
       res.json({ items: null, error });
-      console.log(error)
+      console.log(error);
     });
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}, visit http://localhost:${PORT}`);
 });
-
-
-function sanitiseNum(input) {
-  let num = 1;
-  if (typeof input !== "number") {
-    input = 1;
-  }
-
-  num = Math.round(input);
-
-  // num between 0-50
-  num = Math.min(50, input);
-  num = Math.max(0, num);
-  return num;
-}
